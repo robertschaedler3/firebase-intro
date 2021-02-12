d8817ac80ff434e6e503505d4780796504cea55c
Robert Schaedler III <robertschaedler3@gmail.com>
2021-02-12 15:21:48 -0500

    update app module

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,18 +1,48 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoComponent } from './todo/todo.component';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, TodoComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp({
+      apiKey: 'AIzaSyA44SiEq7G_NnEx2I_Y2_Y-hKfWAZAQk0Y',
+      authDomain: 'ssw-322.firebaseapp.com',
+      projectId: 'ssw-322',
+      storageBucket: 'ssw-322.appspot.com',
+      messagingSenderId: '757002946237',
+      appId: '1:757002946237:web:eec0a7df68d481914819d4',
+      measurementId: 'G-7C2RE14J5L',
+    }),
+    AngularFireAuthModule, // auth
+    AngularFirestoreModule, // firestore
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatInputModule,
+    MatListModule,
+    MatToolbarModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
